c10cf2457271257d40d95705d7a57fef9cdf1f42
TTrung <hotuantrung29@gmail.com>
2024-05-12 23:05:09 +0700

    mongodb

--- EvolutionStrategy/agent/app.py
+++ EvolutionStrategy/agent/app.py
@@ -6,9 +6,15 @@ from sklearn.preprocessing import MinMaxScaler
 import pandas as pd
 from datetime import datetime
 from RLmodel import Agent,skip,Model
-
+from pymongo import MongoClient
+import config
+from bson import json_util
 app = Flask(__name__)
 
+client = MongoClient(config.MONGO_URI) # your connection string
+db = client["StockDB"]
+trading_collection = db["Trading"]
+stock_collection = db["Stock"]
 
 with open('model.pkl', 'rb') as fopen:
     model = pickle.load(fopen)
@@ -31,21 +37,23 @@ agent = Agent(model = model,
 def hello():
     return jsonify({'status': 'OK'})
 
-# @app.route('/reset_agent', method = ['POST'])
-# def reset_agent():
-#     data = request.join
-#     df = pd.read_csv('TWTR.csv')
-#     real_trend = df['Close'].tolist()
-#     parameters = [df['Close'].tolist(), df['Volume'].tolist()]
-#     minmax = MinMaxScaler(feature_range = (100, 200)).fit(np.array(parameters).T)
-#     scaled_parameters = minmax.transform(np.array(parameters).T).T.tolist()
-#     initial_money = np.max(parameters[0]) * 2
+@app.route('/reset_agent', methods = ['POST'])
+def reset_agent():
+    data = request.json
+    df = pd.read_csv('TWTR.csv')
+    real_trend = df['Close'].tolist()
+    parameters = [df['Close'].tolist(), df['Volume'].tolist()]
+    minmax = MinMaxScaler(feature_range = (100, 200)).fit(np.array(parameters).T)
+    scaled_parameters = minmax.transform(np.array(parameters).T).T.tolist()
+    initial_money = data.money
 
-#     agent.change_data(timeseries = scaled_parameters,
-#                   skip = skip,
-#                   initial_money = initial_money,
-#                   real_trend = real_trend,
-#                   minmax = minmax)
+    agent.change_data(timeseries = scaled_parameters,
+                  skip = skip,
+                  initial_money = initial_money,
+                  real_trend = real_trend,
+                  minmax = minmax)
+    
+    return jsonify(True)
 
 @app.route('/inventory', methods = ['GET'])
 def inventory():
@@ -96,9 +104,42 @@ def trade_range():
     for date, close, volume in data_list:
         result = agent.trade([date,close, volume])
         trade_results.append(result)
-    
+
+    trading_collection.insert_one({
+        "UserId": data.get('UserId'),
+        "StockSymbol": data.get('Symbol'),
+        "HistoryTrading":trade_results
+    }) 
     return jsonify(trade_results)
 
+
+@app.route('/trade_history', methods = ['GET'])
+def trade_historry():
+    userId = request.args.get('UserId')
+    symbol = request.args.get('StockSymbol')
+
+    if isinstance(userId, str):
+            userId = int(userId)
+
+    if not userId or not symbol:
+        return jsonify({'error': 'UserId and Symbol parameters are required'}), 400
+    print("userid,symbol",userId,symbol)
+
+    document  =  trading_collection.find_one({
+        "UserId": userId,
+        "StockSymbol": symbol
+    })
+    
+    return jsonify(json_util.dumps(document))
+
+
+@app.route('/all_trade_history',methods = ['GET'])
+def all_trade_history():
+    all_documents = list(trading_collection.find())
+   
+    for doc in all_documents:
+        doc['_id'] = str(doc['_id'])
+    return jsonify(json_util.dumps(all_documents))
 
 @app.route('/reset', methods = ['GET'])
 def reset():
